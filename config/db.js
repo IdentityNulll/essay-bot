817f4851f90a74be51445e009d68907e2a28f607
IdentityNulll <identitynulll@gmail.com>
2026-07-02 16:35:44 +0500

    removed db

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ielts_bot'
 export const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log('MongoDB connected successfully to:', MONGO_URI);
+    console.log('MongoDB connected successfully to:');
   } catch (error) {
     console.error('MongoDB connection error:', error.message);
     process.exit(1);
